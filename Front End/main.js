cef6eba40b3418fac9d8f40f9b9c4a4fa39911dc
Sid <sid6327@gmail.com>
2017-12-23 11:17:21 +0530

    Adding range sliders and panel.

--- Front End/main.js
+++ Front End/main.js
@@ -1,7 +1,8 @@
-var audioContext, searchRequest, searchDisplay, currentSong
+var audioContext, searchRequest, searchDisplay, currentSong, gainNode
 
 window.onload = function() {
 	audioContext = new (window.AudioContext || window.webkitAudioContext)()
+	gainNode = audioContext.createGain()
 	searchRequest = new XMLHttpRequest()
 	searchRequest.addEventListener("load", searchComplete);
     searchRequest.addEventListener("error", searchFailed);
@@ -29,6 +30,8 @@ function playSong(id) {
 		var analyser = audioContext.createAnalyser()
 		var source = audioContext.createAnalyser()
 		source.connect(analyser)
+		source.connect(gainNode)
+		gainNode.connect(audioContext.destination)
 		analyser.connect(audioContext.destination)
 		// Remove button and add audio source
 		var card = document.getElementById(id)
@@ -45,6 +48,7 @@ function playSong(id) {
 		// Insert card
 		var songCard = document.getElementById(id).cloneNode(true)
 		songCard.appendChild(audio)
+
 		// Add Panel here
 		var btn = document.createElement('button')
 		btn.innerHTML = 'Panel'
@@ -53,25 +57,31 @@ function playSong(id) {
 		btn.setAttribute('data-target', `#panel${id}`)
 		songCard.appendChild(document.createElement('br'))
 		songCard.appendChild(btn)
-		songCard.appendChild(makeAudioPanel(id))
 		div.appendChild(songCard)
+
 		if (currentSong) {
 			currentSong.pause()
 			currentSong.currentTime = 0
 		}
 		currentSong = audio
-		// Add onplay event and play
+		audio.volume = 0.1
 		audio.play()
+
+		audio.addEventListener('canplaythrough', function() { 
+			songCard.appendChild(makeAudioPanel(id, audio))
+		}, false);
+
+		// Add onplay event and play
 		audio.onplay = () => {
-			if (currentSong != audio) {
+			if (currentSong && currentSong != audio) {
 				currentSong.pause()
 				currentSong.currentTime = 0
 				currentSong = audio
-				fillRelated(id);
+				fillRelated(id)
 			}
 		}
+		songCard.removeChild(songCard.childNodes[songCard.childNodes.length - 4])
 		// Only keep limited number of active songs on player
-		songCard.removeChild(songCard.childNodes[songCard.childNodes.length - 5])
 		player.insertBefore(div, player.childNodes[0])
 		if (player.childNodes.length == 10) {
 			player.removeChild(player.childNodes[9])
@@ -154,9 +164,28 @@ function getSearchResults() {
 
 /* ------ Utility functions ------ */ 
 
+function getTimeString(seconds) {
+	let minutes = parseInt(seconds / 60)
+	let hours = parseInt(minutes / 60)
+	if (minutes < 10) {
+		minutes = '0' + minutes
+	}
+	seconds = parseInt(seconds % 60)
+	if (seconds < 10) {
+		seconds = '0' + seconds
+	}
+	if (hours) {
+		minutes = parseInt(minutes % 60)
+		if (minutes < 10) {
+			minutes = '0' + minutes
+		}
+		return hours + ':' + minutes + ':' + seconds
+	}
+	return minutes + ':' + seconds
+}
+
 function searchComplete() {
 	console.log('Search successful')
-	console.log(searchRequest.response)
 	// Animate tab
 	document.getElementById('searchResults').style.animation = 'colorChange 1s'
 	setTimeout(() => {
@@ -196,10 +225,134 @@ function makeCard(cardTitle, id, cardSnippet) {
 	return div
 }
 
-function makeAudioPanel(id) {
+function makeAudioPanel(id, audio) {
 	var div = document.createElement('div')
 	div.setAttribute('id', `panel${id}`)
 	div.setAttribute('class', `collapse`)
-	div.innerHTML = 'Lorem ipsum dolor text....'
+	// div.style.marginLeft = '10px'
+	div.style.background = '#e8ecf2'
+	div.style.borderRadius = '25px'
+	div.style.paddingLeft = '5px'
+	div.style.paddingRight = '5px'
+	div.appendChild(document.createElement('br'))
+	// Precise Seek
+	let h6 = document.createElement('h6')
+	h6.innerHTML = 'Seek:'
+	div.appendChild(h6)
+	var seekSlider = document.createElement('div')
+	noUiSlider.create(seekSlider, {
+		start: [ 0 ],
+		connect: true,
+		range: {
+			'min': [  0 ],
+			'max': [ audio.duration ]
+		}
+	})
+
+	var seekSliderValueElement = document.createElement('div')
+	seekSlider.appendChild(seekSliderValueElement)
+	seekSliderValueElement.innerHTML = 0
+
+	seekSlider.noUiSlider.on('update', (values, handle) => {
+		seekSliderValueElement.innerHTML = getTimeString(values[handle])
+		
+	})
+
+	seekSlider.noUiSlider.on('end', (value) => {
+		audio.currentTime = value
+	});
+
+	let range = []
+	var rangeSlider = document.createElement('div')
+	var rangeSliderValueElement = document.createElement('div')
+	// Range Loop
+	noUiSlider.create(rangeSlider, {
+		start: [ 0, audio.duration ],
+		connect: [ false, true, false ],
+		range: {
+			'min': [ 0 ],
+			'max': [ audio.duration ]
+		}
+	});
+
+	rangeSlider.noUiSlider.on('update', (values, handle) => {
+		rangeSliderValueElement.innerHTML = getTimeString(values[0]) + ' - ' + getTimeString(values[1])
+	})
+
+	rangeSlider.noUiSlider.on('end', (values) => {
+		rangeSliderValueElement.innerHTML = getTimeString(values[0]) + ' - ' + getTimeString(values[1])
+		range[0] = values[0]
+		range[1] = values[1]
+	});
+
+	rangeSlider.appendChild(rangeSliderValueElement)
+	rangeSliderValueElement.innerHTML = getTimeString(0) + ' - ' + getTimeString(audio.duration)
+
+	div.appendChild(seekSlider)
+	div.appendChild(document.createElement('br'))
+
+	h6 = document.createElement('h6')
+	h6.innerHTML = 'Range:'
+	h6.style.marginTop = '15px'
+	div.appendChild(h6)
+
+	let loopBtn = document.createElement('button')
+	loopBtn.innerHTML = 'Loop'
+	loopBtn.setAttribute('class', 'btn btn-sm btn-success')
+	loopBtn.onclick = loopFunction.bind(null, audio)
+	rangeSlider.appendChild(loopBtn)
+
+	let saveBtn = document.createElement('button')
+	saveBtn.innerHTML = 'Save'
+	saveBtn.setAttribute('class', 'btn btn-sm btn-info')
+	saveBtn.style.marginLeft = '5px'
+	rangeSlider.appendChild(saveBtn)
+
+	div.appendChild(rangeSlider)
+	div.appendChild(document.createElement('br'))
+	div.appendChild(document.createElement('br'))
+
+	// Add Volume boost
+	h6 = document.createElement('h6')
+	h6.innerHTML = 'Volume Boost:'
+	h6.style.marginTop = '15px'
+
+	div.appendChild(h6)
+
+	let stepSlider = document.createElement('div')
+	noUiSlider.create(stepSlider, {
+		start: [ 1 ],
+		step: 0.2,
+		range: {
+			'min': [ 1 ],
+			'max': [ 2 ]
+		}
+	})
+
+	div.appendChild(stepSlider)
+	div.appendChild(document.createElement('br'))
 	return div
+	
+}
+
+function loopFunction(audio) {
+	console.log(audio.duration)
+	gainNode.gain.setValueAtTime(0, audioContext.currentTime)
+	// gainNode.gain.exponentialRampToValueAtTime(0.01, audioContext.currentTime + 10)
+	gainNode.gain.value = 0.01
+}
+
+function getLocal() {
+	var request = new XMLHttpRequest()
+	let div = document.getElementById('tempLocal')
+	request.addEventListener('load', continuePlaying)
+	request.addEventListener('error', errorPlaying)
+	request.open('GET', `file:///storage/emulated/0/Download/`, true)
+	request.send()
+	function continuePlaying() {
+		div.innerHTML = request.response
+	}
+	function errorPlaying() {
+		div.innerHTML = request.response
+	}
 }
